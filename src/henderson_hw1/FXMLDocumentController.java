748742e16f82e2c7d16b8abac787e1adf81be592
gxhenderson <gxhenderson@live.com>
2015-09-13 19:49:18 -0400

    Update

--- src/henderson_hw1/FXMLDocumentController.java
+++ src/henderson_hw1/FXMLDocumentController.java
@@ -7,6 +7,7 @@ package henderson_hw1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -30,17 +31,26 @@ public class FXMLDocumentController implements Initializable
     @FXML
     private ComboBox cbxExtraTopping;
     
+    @FXML
+    private TableView tblPizza;
+    
+    private ObservableList<Pizza> pizzas;
     
     @FXML
     private void btnAddPressed(ActionEvent event) 
     {
-        
+        pizzas.add(new Pizza(txtLastName.getText(), 
+                (String)cbxPizzaName.getValue(), 
+                (String)cbxPizzaSize.getValue(), 
+                (String)cbxExtraTopping.getValue(),
+                Pizza.getPrice((String)cbxPizzaName.getValue(), 
+                        (String)cbxPizzaSize.getValue())));
     }
     
     @FXML
     private void btnOrderPressed(ActionEvent event) 
     {
-        
+        tblPizza.setItems(pizzas);
     }
     
     @Override
@@ -49,4 +59,102 @@ public class FXMLDocumentController implements Initializable
         
     }    
     
+    public static class Pizza
+    {
+        private final String lastName;
+        private final String pizzaName;
+        private final String pizzaSize;
+        private final String extraTopping;
+        private final double price;
+                
+        //Pepperoni Prices
+        private final static double PEP = 5.00;
+        
+        //Hawaiian Prices
+        private final static double HAW = 7.00;
+        
+        //Veggie Prices
+        private final static double VEG = 6.00;
+        
+        //Meat Prices
+        private final static double MEAT = 7.00;
+        
+        //Special Prices
+        private final static double SPEC = 8.00;
+        
+        //Size Charges
+        private final static double MED = 2.00;
+        private final static double LARGE = 4.00;
+        
+        //Topping Price
+        private final static double TOPSMALL = 0.50;
+        private final static double TOPMED = 1.00;
+        private final static double TOPLARGE = 1.50;
+        
+        /**
+         *
+         * @param lastName
+         * @param pizzaName
+         * @param pizzaSize
+         * @param extraTopping
+         * @param price
+         */
+        public Pizza(String lastName, String pizzaName, String pizzaSize,
+                String extraTopping, double price)
+        {
+            this.lastName = lastName;
+            this.pizzaName = pizzaName;
+            this.pizzaSize = pizzaSize;
+            this.extraTopping = extraTopping;
+            this.price = price;
+        }
+        
+        /**
+         *
+         * @param pizzaName
+         * @param pizzaSize
+         * @return
+         */
+        public static double getPrice(String pizzaName, String pizzaSize)
+        {
+            double tempPrice = 0.00;
+            
+            switch(pizzaName)
+            {
+                case "Pepperoni": 
+                    tempPrice += PEP;
+                    break;
+                case "Hawaiian":
+                    tempPrice += HAW;
+                    break;
+                case "Veggie":
+                    tempPrice += VEG;
+                    break;
+                case "Meat":
+                    tempPrice += MEAT;
+                    break;
+                case "Special":
+                    tempPrice += SPEC;
+                    break;
+            }
+            
+            //Adds additional fees for different size pizzas,
+            //including topping fees
+            switch(pizzaSize)
+            {
+                case "Medium":
+                    tempPrice += MED + TOPMED;
+                    break;
+                case "Large":
+                    tempPrice += LARGE + TOPLARGE;
+                    break;
+                //If the pizza is small
+                default:
+                    tempPrice += TOPSMALL;
+                    break;
+            }
+            
+            return tempPrice;
+        }
+    } 
 }
