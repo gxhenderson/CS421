7fc9962a616cfd0e3c3998511f26e730a3b11d4b
gxhenderson <gxhenderson@live.com>
2015-09-13 18:42:29 -0400

    Base Commit

---
+++ src/henderson_hw1/FXMLDocumentController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package henderson_hw1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+/**
+ *
+ * @author gxhender
+ */
+public class FXMLDocumentController implements Initializable 
+{
+    @FXML
+    private TextField txtLastName;
+    
+    @FXML
+    private ComboBox cbxPizzaName;
+    
+    @FXML
+    private ComboBox cbxPizzaSize;
+    
+    @FXML
+    private ComboBox cbxExtraTopping;
+    
+    
+    @FXML
+    private void btnAddPressed(ActionEvent event) 
+    {
+        
+    }
+    
+    @FXML
+    private void btnOrderPressed(ActionEvent event) 
+    {
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        
+    }    
+    
+}
